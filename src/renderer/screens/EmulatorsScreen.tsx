f4527f6adadf1ff9c5a491e467b8858a4521f37c
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-07-12 07:12:32 +0900

    💄: 各種ボタン表示とレイアウト調整

--- src/renderer/screens/EmulatorsScreen.tsx
+++ src/renderer/screens/EmulatorsScreen.tsx
@@ -1,5 +1,4 @@
-import {Typography} from '@mui/material';
-import {Box} from '@mui/system';
+import {Button, List, ListItem, Stack, Typography} from '@mui/material';
 import React from 'react';
 import {useEmulators} from 'renderer/state/useEmulators';
 
@@ -7,10 +6,23 @@ export const EmulatorsScreen: React.FC = () => {
   const emulators = useEmulators(state => state.emulators);
 
   return (
-    <Box>
+    <List sx={{height: '100vh', backgroundColor: '#444444', overflow: 'auto'}}>
       {emulators.map(emu => (
-        <Typography key={emu}>{emu}</Typography>
+        <ListItem key={emu}>
+          <Stack direction="row" sx={{alignItems: 'center', justifyContent: 'space-between', width: '100%'}}>
+            <Typography color="white">{emu}</Typography>
+            <Stack direction="row" sx={{gap: 2}}>
+              <Button variant="outlined" color="success">
+                Boot
+              </Button>
+              <Button variant="outlined">Cold Boot</Button>
+              <Button variant="outlined" color="error">
+                Wipe Data Boot
+              </Button>
+            </Stack>
+          </Stack>
+        </ListItem>
       ))}
-    </Box>
+    </List>
   );
 };
